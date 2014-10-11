8743aa53e461cfefd0829d254d3a48d93a5fadef
Xue Qian <xueqian0321@126.com>
2014-10-11 21:06:51 +0800

    add readFile function test and FileProcessor.java

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/FileProcessor.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.iamcoach.pos;
+
+import java.util.List;
+
+public class FileProcessor {
+    public List readFile(String fileName) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/FileProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/FileProcessorTest.java
@@ -1,11 +1,16 @@
 package com.thoughtworks.iamcoach.pos;
 
 import org.junit.Test;
-,
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class FileProcessorTest {
     @Test
     public void should_get_a_list(){
-
-
+        FileProcessor fileProcessor = mock(FileProcessor.class);
+        when(fileProcessor.readFile("items.txt")).thenReturn(null);
+        assertThat(fileProcessor.readFile("items.txt")).isEqualTo(null);
     }
 }
