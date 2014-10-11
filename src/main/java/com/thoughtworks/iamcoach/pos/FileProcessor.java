5ee2e6aa7f6988717837a71a923d6429db913ab8
Xue Qian <xueqian0321@126.com>
2014-10-11 22:50:53 +0800

    add getFilePath function and test pass

--- src/main/java/com/thoughtworks/iamcoach/pos/FileProcessor.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/FileProcessor.java
@@ -1,13 +1,22 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileProcessor {
+
     public List readFile(String fileName) {
+
         List<String> data = new ArrayList<String>();
         data.add(0,"qqq");
         data.add(1,"www");
         return data;
+    }
+
+    private static String getFilePath(String fileName) {
+
+        File file = new File(fileName);
+        return file.getAbsolutePath();
     }
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/FileProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/FileProcessorTest.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,13 +12,28 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class FileProcessorTest {
+
+    private static String getFilePath(String fileName) {
+
+        File file = new File(fileName);
+        return file.getAbsolutePath();
+    }
+
     @Test
     public void should_get_a_list(){
-        FileProcessor fileProcessor = mock(FileProcessor.class);
+
         List<String> data = new ArrayList<String>();
+        FileProcessor fileProcessor = mock(FileProcessor.class);
         data.add(0,"aaa");
         data.add(1,"eee");
         when(fileProcessor.readFile("items.txt")).thenReturn(data);
         assertThat(fileProcessor.readFile("items.txt")).isEqualTo(data);
     }
+
+    @Test
+    public void should_get_abusolute_path()throws Exception{
+        assertThat(getFilePath("cart.txt")).isEqualTo("/home/xueqian/java_projects/pos-java/cart.txt");
+
+    }
+
 }
