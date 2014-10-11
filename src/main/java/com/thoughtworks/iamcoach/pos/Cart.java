efff2097912a4253425fbfce6968ec7d1a16051a
Xue Qian <xueqian0321@126.com>
2014-10-11 09:14:06 +0800

    add Cart.java

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/Cart.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.iamcoach.pos;
+
+public class Cart {
+    private String barcode;
+    private double count;
+
+    public Cart(){
+
+    }
+    
+    public Cart(String barcode,double count){
+        this.barcode = barcode;
+        this.count = count;
+    }
+
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public double getCount() {
+        return count;
+    }
+
+    public void setCount(double count) {
+        this.count = count;
+    }
+}
