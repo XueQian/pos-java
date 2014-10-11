10412436ffac567ceba3ba92c1ff427c7f190111
Xue Qian <xueqian0321@126.com>
2014-10-11 10:00:38 +0800

    modify Items.java to Item.java

--- src/main/java/com/thoughtworks/iamcoach/pos/Item.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Item.java
@@ -1,16 +1,16 @@
 package com.thoughtworks.iamcoach.pos;
 
-public class Items {
+public class Item {
     private String barcode;
     private String name;
     private double price;
     private String unit;
 
-    public Items(){
+    public Item(){
 
     }
 
-    public Items(String barcode, String name, double price, String unit) {
+    public Item(String barcode, String name, double price, String unit) {
         this.barcode = barcode;
         this.name = name;
         this.price = price;
