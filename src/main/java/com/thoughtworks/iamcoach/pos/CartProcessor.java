e0674f35c7e18031ecdbf6182b9965f17aa2b840
Xue Qian <xueqian0321@126.com>
2014-10-12 11:44:10 +0800

    CartProcessor can return Cart List and test pass

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/CartProcessor.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.iamcoach.pos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartProcessor {
+
+    public List<Cart> process(){
+
+        List<Cart> carts = new ArrayList<Cart>();
+        return carts;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/CartProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CartProcessorTest.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CartProcessorTest {
+
+    @Test
+    public void should_return_a_cart_list(){
+
+        CartProcessor cartProcessor = mock(CartProcessor.class);
+        List<Cart> carts = new ArrayList<Cart>();
+        carts.add(0,new Cart("ITEM000001",1));
+
+        when(cartProcessor.process()).thenReturn(carts);
+        assertThat(cartProcessor.process().get(0).getBarcode()).isEqualTo("ITEM000001");
+    }
 }
