acd80c4a5f1eb0b8b640ab9dcd8bcf6402285cae
Xue Qian <xueqian0321@126.com>
2014-10-12 16:10:53 +0800

    add PromotionProcessor can return an promotion list with one parament test and file

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionProcessor.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.iamcoach.pos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromotionProcessor {
+
+    public List<Promotion> halfProcess() {
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        return  promotions;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionProcessorTest.java
@@ -1,4 +1,27 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PromotionProcessorTest {
+
+    @Test
+    public void should_return_an_promotion_object_with_one_parament(){
+        PromotionProcessor promotionProcessor = mock(PromotionProcessor.class);
+        List<Promotion> promotions = new ArrayList<Promotion>();
+        promotions.add(0,new Promotion("IE00000"));
+        when(promotionProcessor.halfProcess()).thenReturn(promotions);
+        assertThat(promotionProcessor.halfProcess()).isEqualTo(promotions);
+
+    }
+
+
+
+
 }
