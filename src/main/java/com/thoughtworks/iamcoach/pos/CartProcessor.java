833a77025d377250fbdf37d717019a4653c7d5a5
Xue Qian <xueqian0321@126.com>
2014-10-12 15:37:59 +0800

    modify ItemProcessor file and test

--- src/main/java/com/thoughtworks/iamcoach/pos/CartProcessor.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/CartProcessor.java
@@ -1,13 +1,30 @@
 package com.thoughtworks.iamcoach.pos;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class CartProcessor {
+public class CartProcessor extends FileProcessor {
 
-    public List<Cart> process(){
+    public List<Cart> process() {
 
         List<Cart> carts = new ArrayList<Cart>();
+        List<String> cartList = readFile("cart.txt");
+
+        for (String aCartList : cartList) {
+            if (aCartList.length() > 10) {
+                String stringBarcode = aCartList.split("-")[0];
+                String stringCount = aCartList.substring(11);
+                Cart cart = new Cart(stringBarcode, Double.parseDouble(stringCount));
+                carts.add(cart);
+            }
+        }
+
+        Set<String> uniqueBarcode = new HashSet<String>(cartList);
+        for(String barcode : uniqueBarcode){
+                Cart cart = new Cart(barcode,Collections.frequency(cartList,barcode));
+                carts.add(cart);
+            }
         return carts;
     }
+
 }
+
