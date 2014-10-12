9c28bb9234bfeea093f59c12fca5582d1145ce10
Xue Qian <xueqian0321@126.com>
2014-10-12 15:38:57 +0800

    modify ItemProcessot.java

--- src/main/java/com/thoughtworks/iamcoach/pos/ItemProcessor.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/ItemProcessor.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemProcessor extends FileProcessor {
-        public List<Item> process(){
+    public List<Item> process(){
         List<Item> items = new ArrayList<Item>();
         List<String> itemList = readFile("items.txt");
 
