7b59fb4862d19da93e3f6bb321995456ec996f36
Xue Qian <xueqian0321@126.com>
2014-10-12 21:10:00 +0800

    add getItem through barcode test

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/Scanner.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.iamcoach.pos;
+
+public class Scanner {
+    public Item getItem() {
+        return null;
+    }
+
+
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ScannerTest.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.iamcoach.pos;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class ScannerTest {
+
+    @Test
+    public void should_get_item_by_barcode(){
+        Scanner scanner = mock(Scanner.class);
+
+        Item item = new Item("ITEM000001", "可乐", 11, "瓶");
+
+        when(scanner.getItem()).thenReturn(item);
+        assertThat(scanner.getItem().getBarcode()).isEqualTo("ITEM000001");
+    }
+
 }
