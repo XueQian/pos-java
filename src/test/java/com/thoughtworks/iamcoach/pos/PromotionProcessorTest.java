9f3bceb167949e2d3303d22a509d36ceb72d1573
Xue Qian <xueqian0321@126.com>
2014-10-20 19:04:22 +0800

    add
    should_return_second_half_price_promotion_list test

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionProcessorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionProcessorTest.java
@@ -40,12 +40,18 @@ public class PromotionProcessorTest {
     }
 
     @Test
+    public void should_return_second_half_price_promotion_list(){
+        PromotionProcessor promotionProcessor = new PromotionProcessor();
+        assertThat(promotionProcessor.halfProcess().get(2).getBarcode()).isEqualTo("ITEM000005");
+
+    }
+
+    @Test
     public void should_return_discount_promotion_list(){
         PromotionProcessor promotionProcessor = new PromotionProcessor();
         assertThat(promotionProcessor.discountProcess().get(2).getBarcode()).isEqualTo("ITEM000005");
         assertThat(promotionProcessor.discountProcess().get(1).getDiscount()).isEqualTo(85);
 
     }
-
 
 }
