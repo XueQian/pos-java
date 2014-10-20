cf9bfb8b0a238f0b801b8c806f4d297cbbf16e21
Xue Qian <xueqian0321@126.com>
2014-10-20 21:10:25 +0800

    finish getSubtotal function and test

--- src/main/java/com/thoughtworks/iamcoach/pos/Calculator.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Calculator.java
@@ -1,7 +1,29 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Calculator {
+
+    PromotionCalculator promotionCalculator = new PromotionCalculator();
     public double getSubtotal(CartItem cartItem) {
-        return 40;
+
+        List<Double> promotionList = new ArrayList<Double>();
+
+        if (promotionCalculator.calculateDiscount(cartItem) > 0) {
+            promotionList.add(promotionCalculator.calculateDiscount(cartItem));
+        }
+
+        if (promotionCalculator.calculateOneFree(cartItem) > 0) {
+            promotionList.add(promotionCalculator.calculateOneFree(cartItem));
+        }
+
+        if (promotionCalculator.calculateSecondHalf(cartItem) > 0) {
+            promotionList.add(promotionCalculator.calculateSecondHalf(cartItem));
+        }
+
+        return Collections.min(promotionList);
     }
+    
 }
