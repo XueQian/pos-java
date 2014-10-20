e704372d03a346bfec7d17e2d69421a424e66653
Xue Qian <xueqian0321@126.com>
2014-10-20 21:44:15 +0800

    modify Calculator.java

--- src/main/java/com/thoughtworks/iamcoach/pos/Calculator.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Calculator.java
@@ -10,18 +10,22 @@ public class Calculator {
 
         PromotionCalculator promotionCalculator = new PromotionCalculator();
 
+        double discountPrice = promotionCalculator.calculateDiscount(cartItem);
+        double oneFreePrice = promotionCalculator.calculateOneFree(cartItem);
+        double secondHalfPrice = promotionCalculator.calculateSecondHalf(cartItem);
+
         List<Double> promotionList = new ArrayList<Double>();
 
-        if (promotionCalculator.calculateDiscount(cartItem) > 0) {
-            promotionList.add(promotionCalculator.calculateDiscount(cartItem));
+        if (discountPrice > 0) {
+            promotionList.add(discountPrice);
         }
 
-        if (promotionCalculator.calculateOneFree(cartItem) > 0) {
-            promotionList.add(promotionCalculator.calculateOneFree(cartItem));
+        if (oneFreePrice > 0) {
+            promotionList.add(oneFreePrice);
         }
 
-        if (promotionCalculator.calculateSecondHalf(cartItem) > 0) {
-            promotionList.add(promotionCalculator.calculateSecondHalf(cartItem));
+        if (secondHalfPrice > 0) {
+            promotionList.add(secondHalfPrice);
         }
 
         return Collections.min(promotionList);
@@ -41,4 +45,6 @@ public class Calculator {
         }
         return totalMoney;
     }
+
+
 }
