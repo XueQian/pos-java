bf89b880d0c0b7e8c791def7d39302120dfe3a40
Xue Qian <xueqian0321@126.com>
2014-10-20 19:34:04 +0800

    finish calculateSecondHalf method and test pass

--- src/main/java/com/thoughtworks/iamcoach/pos/PromotionCalculator.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/PromotionCalculator.java
@@ -15,6 +15,11 @@ public class PromotionCalculator {
         return discountMoney;
     }
 
+    public double calculateSecondHalf(CartItem cartItem) {
+        double secondHalfMoney = cartItem.getCount()*cartItem.getItem().getPrice() - (int)(cartItem.getCount()/3)*cartItem.getItem().getPrice()/2;
+        return secondHalfMoney;
+    }
+
     private double getDiscount(String barcode){
         PromotionProcessor promotionProcessor = new PromotionProcessor();
         List<Promotion> discountList = promotionProcessor.discountProcess();
@@ -26,4 +31,5 @@ public class PromotionCalculator {
         }
         return 0.0;
     }
+
 }
--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionCalculatorTest.java
@@ -31,5 +31,12 @@ public class PromotionCalculatorTest {
         assertThat(promotionCalculator.calculateOneFree(cartItem)).isEqualTo(220.0);
     }
 
+    @Test
+    public void should_return_total_money_with_second_half_price_promotion(){
+        PromotionCalculator promotionCalculator = new PromotionCalculator();
+        Item item = new Item("ITEM000005","鸡翅",10,"块");
+        CartItem cartItem = new CartItem(item,5);
+        assertThat(promotionCalculator.calculateSecondHalf(cartItem)).isEqualTo(45);
+    }
 
 }
