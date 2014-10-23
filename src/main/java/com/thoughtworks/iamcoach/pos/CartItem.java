b043b13c2bcd2dc7a9b112aa44ec2d605f940ad9
Xue Qian <xueqian0321@126.com>
2014-10-23 19:27:44 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/CartItem.java
@@ -4,10 +4,6 @@ public class CartItem {
     private Item item;
     private double count;
 
-    public CartItem() {
-
-    }
-
     public CartItem(Item item, double count) {
         this.item = item;
         this.count = count;
@@ -17,15 +13,8 @@ public class CartItem {
         return item;
     }
 
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
     public double getCount() {
         return count;
     }
 
-    public void setCount(double count) {
-        this.count = count;
-    }
 }
