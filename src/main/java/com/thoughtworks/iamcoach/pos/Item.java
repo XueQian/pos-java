96b2f4e448d486710e15ac6224684959d3818879
Xue Qian <xueqian0321@126.com>
2014-10-23 19:29:41 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/Item.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Item.java
@@ -6,10 +6,6 @@ public class Item {
     private double price;
     private String unit;
 
-    public Item() {
-
-    }
-
     public Item(String barcode, String name, double price, String unit) {
         this.barcode = barcode;
         this.name = name;
@@ -21,32 +17,16 @@ public class Item {
         return barcode;
     }
 
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public String getUnit() {
         return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
     }
 
 }
